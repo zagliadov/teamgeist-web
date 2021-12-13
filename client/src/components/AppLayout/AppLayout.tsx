dfb8fffc121c89d24423ac6dcc8ed40677bdc8e9
Daniil <portal.sid.grand2@gmail.com>
2021-12-13 09:05:52 +0200

    Added options for the developer + checking for user type from localStorage and from the location (state)

--- client/src/components/AppLayout/AppLayout.tsx
+++ client/src/components/AppLayout/AppLayout.tsx
@@ -1,29 +1,36 @@
 import { FC } from 'react';
 import AppHeader from '../AppHeader/AppHeader';
 import AppContent from '../AppContent/AppContent';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Layout } from 'antd';
 
-const headerOptions = [
+const headerOptionsForAdmin = [
     { id: 0, name: 'Пользователи', to: '/admin/users' },
     { id: 1, name: 'Проекты', to: '/admin/projects' },
     { id: 2, name: 'Статистика', to: '/admin/statistics' },
     { id: 3, name: 'Отчеты о собоях', to: '/admin/crash_reports' },
-    { id: 4,name: 'Выход', to: '/login' },
-]
+    { id: 4, name: 'Выход', to: '/login' },
+];
+const headerOptionsForDeveloper = [
+    { id: 0, name: 'Список проектов', to: '/developer/project-list' },
+    { id: 1, name: 'Выход', to: '/login' },
+];
 
 const AppLayout: FC = () => {
     const userType = localStorage.getItem('user');
-    console.log(userType);
+    const location = useLocation();
+
     return (
         <Layout style={{
             minHeight: '100vh',
             display: 'flex',
             alignItems: 'center',
         }}>
-            {userType === 'admin' && <AppHeader headerOption={headerOptions} />}
-
-
+            {(userType === 'admin' || location?.state === 'admin')
+                && <AppHeader headerOption={headerOptionsForAdmin} />}
+            {(userType === 'developer' || location?.state === 'developer')
+                && <AppHeader headerOption={headerOptionsForDeveloper} />}
+                
             <AppContent>
                 <Outlet />
             </AppContent>
