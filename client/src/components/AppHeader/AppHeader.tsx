f465eb32fc225e0c394952f3ea9089e05dceb299
Daniil <portal.sid.grand2@gmail.com>
2021-12-13 09:26:47 +0200

    Moved the Exit button into a separate block

--- client/src/components/AppHeader/AppHeader.tsx
+++ client/src/components/AppHeader/AppHeader.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Col, Layout, Row } from 'antd';
+import { Col, Layout, Menu, Row } from 'antd';
 import Logo from '../Logo/Logo';
 import { NavLink } from 'react-router-dom';
 const { Header } = Layout;
@@ -7,7 +7,7 @@ const { Header } = Layout;
 
 const AppHeader: FC<any> = ({ headerOption }) => {
 
-    
+
 
     return (
         <Header style={{
@@ -26,20 +26,32 @@ const AppHeader: FC<any> = ({ headerOption }) => {
                     <Logo />
                 </Col>
 
-                <Col span={21}>
-                    {headerOption.map((item: any) => {
-                        return (
-                            <NavLink to={item.to} key={item.id} 
-                                className={({ isActive }) => isActive ? 'active' : 'link'} >
-                                {item.name}
-                            </NavLink>
-                        )
-                    })}
+                <Col span={18}>
+                    <Menu theme="light" mode='horizontal' defaultSelectedKeys={['5']} className='menu'>
+                        {headerOption.map((item: any) => {
+                            return (
+                                <Menu.Item key={item.id} className='menu__item' >
+                                    <NavLink to={item.to} key={item.id}
+                                        className={({ isActive }) => isActive ? 'active' : 'link'} >
+                                        {item.name}
+                                    </NavLink>
+                                </Menu.Item>
+
+                            )
+                        })}
+                    </Menu>
                 </Col>
 
-
-
-
+                <Col span={3} style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    borderLeft: '2px solid rgba(167, 167, 167, 0.376)',
+                }}>
+                    <NavLink to='/login'
+                        className={({ isActive }) => isActive ? 'active' : 'link'} >
+                        Выход
+                    </NavLink>
+                </Col>
             </Row>
         </Header>
     )
@@ -47,41 +59,3 @@ const AppHeader: FC<any> = ({ headerOption }) => {
 
 export default AppHeader;
 
-
-
-// <Col span={18}>
-
-// <Menu theme="light" mode='horizontal' defaultSelectedKeys={['5']} className='menu'>
-//     <Menu.Item key="1" className='menu__item' >
-//         <NavLink to="/admin/users" className={({ isActive }) => isActive ? 'active' : 'link'} >
-//             Пользователи
-//         </NavLink>
-//     </Menu.Item>
-
-//     <Menu.Item key="2" className='menu__item'>
-//         <NavLink to='/admin/projects' className={({ isActive }) => isActive ? 'active' : 'link'} >
-//             Проекты
-//         </NavLink>
-//     </Menu.Item>
-
-//     <Menu.Item key="3" className='menu__item'>
-//         <NavLink to='/admin/statistic' className={({ isActive }) => isActive ? 'active' : 'link'} >
-//             Статистика
-//         </NavLink>
-//     </Menu.Item>
-
-//     <Menu.Item key="4" className='menu__item'>
-//         <NavLink to='/admin/crash_reports' className={({ isActive }) => isActive ? 'active' : 'link'} >
-//             Отчеты о собоях
-//         </NavLink>
-//     </Menu.Item>
-
-// </Menu>
-// </Col>
-// <Col span={3}>
-// <Menu theme="light" mode='horizontal' defaultSelectedKeys={['1']} className='menu'>
-//     <Menu.Item key="5" className='link' >
-//         Выход
-//     </Menu.Item>
-// </Menu>
-// </Col>
--- client/src/components/AppLayout/AppLayout.tsx
+++ client/src/components/AppLayout/AppLayout.tsx
@@ -9,11 +9,11 @@ const headerOptionsForAdmin = [
     { id: 1, name: 'Проекты', to: '/admin/projects' },
     { id: 2, name: 'Статистика', to: '/admin/statistics' },
     { id: 3, name: 'Отчеты о собоях', to: '/admin/crash_reports' },
-    { id: 4, name: 'Выход', to: '/login' },
+    // { id: 4, name: 'Выход', to: '/login' },
 ];
 const headerOptionsForDeveloper = [
     { id: 0, name: 'Список проектов', to: '/developer/project-list' },
-    { id: 1, name: 'Выход', to: '/login' },
+    // { id: 1, name: 'Выход', to: '/login' },
 ];
 
 const AppLayout: FC = () => {
@@ -30,7 +30,7 @@ const AppLayout: FC = () => {
                 && <AppHeader headerOption={headerOptionsForAdmin} />}
             {(userType === 'developer' || location?.state === 'developer')
                 && <AppHeader headerOption={headerOptionsForDeveloper} />}
-                
+
             <AppContent>
                 <Outlet />
             </AppContent>
