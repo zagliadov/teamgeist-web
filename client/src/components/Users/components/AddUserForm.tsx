fb350e253292383fb513a8f3bbe561e60f86d62c
SunkissedWitch <annagekata82@gmail.com>
2022-01-06 17:25:09 +0200

    Fixed typos

--- client/src/components/Users/components/AddUserForm.tsx
+++ client/src/components/Users/components/AddUserForm.tsx
@@ -103,7 +103,7 @@ const AddUserForm: FC<IPropsSetVisible> = ({ setVisible }) => {
                 label={"Тип пользователя:"}
                 required={true}
                 message={"Please enter valid user type"}
-                placeholder={"Выберить тип пользователя"}
+                placeholder={"Выберите тип пользователя"}
                 className={"input-border"}
                 optionValue={userType.map((item: any) => [{ key: item.key, value: item.userType }])}
             // optionValue={[
@@ -122,7 +122,7 @@ const AddUserForm: FC<IPropsSetVisible> = ({ setVisible }) => {
                 required={false}
                 mode={"tags"}
                 message={"Please enter valid project"}
-                placeholder={"Выберирите проект..."}
+                placeholder={"Выберите проект..."}
                 className={"input-border"}
                 optionValue={project.map((item: any) => [{ key: item.key, value: item.projectName }])}
             // optionValue={[
