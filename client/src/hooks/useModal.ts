175d5049786d5240ab58508d064bd4757f7a52d0
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-01-20 20:10:32 +0200

    added useModal hook

---
+++ client/src/hooks/useModal.ts
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+/**
+* const {
+*   showModal,
+*   handleOk,
+*   handleCancel,
+*   visible,
+*   setVisible,
+*   confirmLoading,
+*  } = useModal(); Скопировать в компонент
+ */
+export const useModal = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+  const [confirmLoading, setConfirmLoading] = useState<boolean>(false);
+
+  const showModal = () => {
+    setVisible(true);
+  };
+  const handleOk = () => {
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setVisible(false);
+  };
+
+  return {
+    showModal,
+    handleOk,
+    handleCancel,
+    visible,
+    setVisible,
+    confirmLoading,
+  };
+};
