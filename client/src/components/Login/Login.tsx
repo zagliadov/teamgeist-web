a94174fe3619a87cfaded17f87a31b1a3822683b
SunkissedWitch <annagekata82@gmail.com>
2022-02-02 15:21:49 +0200

    edit name=email to fix problem with autorization

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -105,7 +105,7 @@ const Login: FC = () => {
                 autoComplete="off"
               >
                 <Form.Item
-                  name="e-mail"
+                  name="email"
                   rules={[{ required: true, message: t('logInForm.PleaseInputYourEmail') }]}
                 >
                   <Input 
