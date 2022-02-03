3eb2d58ccf1d87702787045b52f3baf78435b0ab
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-02-03 14:21:22 +0200

    Removed AuthContext moved logic to AppContext

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import './index.css';
 import { UserProvider } from './state/UserContext';
-import { AuthProvider } from './state/AuthContext';
 import { ProjectProvider } from './state/ProjectContext';
 import { UserTypeProvider } from './state/UserTypeContext';
 import { AppProvider } from './state/AppContext';
@@ -17,9 +16,7 @@ ReactDOM.render(
       <UserProvider>
         <ProjectProvider>
           <AppProvider>
-            <AuthProvider>
               <App />
-            </AuthProvider>
           </AppProvider>
         </ProjectProvider>
       </UserProvider>
